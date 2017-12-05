fbdf94f9affad800f484fb70b844c453fd97a52b
Daniel Raduta <datudar@gmail.com>
2017-12-05 14:49:53 -0600

    Added more risk functionality

--- equity_risk_model.py
+++ equity_risk_model.py
@@ -12,183 +12,187 @@ from sklearn.linear_model import LinearRegression
 
 DATE = '6/30/2017'
 DIR = 'input'
-
 PORT_FILE = '{0}/portfolio.csv'.format(DIR)
 BENCH_FILE = '{0}/benchmark.csv'.format(DIR)
-
-WEIGHT_FILE = '{0}/weight.csv'.format(DIR)
-PRICE_FILE = '{0}/price.csv'.format(DIR)
-SHRSOUT_FILE  = '{0}/shrsout.csv'.format(DIR)
+WGT_FILE = '{0}/weight.csv'.format(DIR)
+PRI_FILE = '{0}/price.csv'.format(DIR)
+SHR_FILE  = '{0}/shrsout.csv'.format(DIR)
 EPS_FILE  = '{0}/eps.csv'.format(DIR)
 
 #==============================================================================
 # Data import
 #==============================================================================
 
-port = pd.read_csv(PORT_FILE, index_col='TICKER', header=0)
-bench = pd.read_csv(BENCH_FILE, index_col='TICKER', header=0)
-active = port - bench
+port = pd.read_csv(PORT_FILE, index_col='TICKER', header=0) # Portfolio
+bench = pd.read_csv(BENCH_FILE, index_col='TICKER', header=0) # Benchmark
+active = port - bench # Active portfolio
 
-weight = pd.read_csv(WEIGHT_FILE, index_col='DATE', header=0)
-price = pd.read_csv(PRICE_FILE, index_col='DATE', header=0)
-shrsout = pd.read_csv(SHRSOUT_FILE, index_col='DATE', header=0)
-eps = pd.read_csv(EPS_FILE, index_col='DATE', header=0)
+wgt = pd.read_csv(WGT_FILE, header=0, index_col='DATE', parse_dates=['DATE'])
+pri = pd.read_csv(PRI_FILE, header=0, index_col='DATE', parse_dates=['DATE'])
+shr = pd.read_csv(SHR_FILE, header=0, index_col='DATE', parse_dates=['DATE'])
+eps = pd.read_csv(EPS_FILE, header=0, index_col='DATE', parse_dates=['DATE'])
 
-#pd.to_datetime(weight.index)
-weight.index = pd.DatetimeIndex(weight.index)
-price.index = pd.DatetimeIndex(price.index)
-shrsout.index = pd.DatetimeIndex(shrsout.index)
-eps.index = pd.DatetimeIndex(eps.index)
+#==============================================================================
+# Create factors
+#==============================================================================
 
-# universe
-price_ret = price.pct_change(periods=1)
-rel_ret = price_ret - np.sum(weight*price_ret)
+# Size factor
+fctr_size = np.log(pri*shr)
+fctr_size_norm = fctr_size.sub(fctr_size.mean(axis=1), axis=0).div(fctr_size.std(axis=1), axis=0)
 
-# size
-size = price * shrsout / 1000000000
-log_size = np.log(size)
-norm_size = log_size.sub(log_size.mean(axis=1), axis=0).div(log_size.std(axis=1), axis=0)
+# Value factor
+fctr_valu  = pd.rolling_sum(eps, window=12, min_periods=None) / pri
+fctr_valu_norm = fctr_valu.sub(fctr_valu.mean(axis=1), axis=0).div(fctr_valu.std(axis=1), axis=0)
 
-# value
-eps_sum = pd.rolling_sum(eps, window=12, min_periods=None)
-eps_yld  = eps_sum / price
-norm_valu = eps_yld.sub(eps_yld.mean(axis=1), axis=0).div(eps_yld.std(axis=1), axis=0)
+# Momentum factor
+fctr_mntm = pri.pct_change(periods=12)
+fctr_mntm_norm = fctr_mntm.sub(fctr_mntm.mean(axis=1), axis=0).div(fctr_mntm.std(axis=1), axis=0)
 
-# momentum
-mom = price.pct_change(periods=12)
-norm_mntm = mom.sub(mom.mean(axis=1), axis=0).div(mom.std(axis=1), axis=0)
+# Stack factors
+stack_pret = pd.DataFrame((pri.pct_change(periods=1) - np.sum(wgt * pri.pct_change(periods=1))).stack())
+stack_fctr_size_norm = pd.DataFrame(fctr_size_norm.stack())
+stack_fctr_valu_norm = pd.DataFrame(fctr_valu_norm.stack())
+stack_fctr_mntm_norm = pd.DataFrame(fctr_mntm_norm.stack())
+stack = pd.concat([stack_pret, stack_fctr_size_norm, stack_fctr_valu_norm, stack_fctr_mntm_norm], axis=1)
+stack.columns = ['pret', 'size', 'valu', 'mntm']
+stack.sort_index(inplace=True)
 
-# stack
-stack_rel_ret = pd.DataFrame(rel_ret.stack())
-stack_norm_size = pd.DataFrame(norm_size.stack())
-stack_norm_valu = pd.DataFrame(norm_valu.stack())
-stack_norm_mntm = pd.DataFrame(norm_mntm.stack())
+#==============================================================================
+# Regression analysis to calculate factor betas
+#==============================================================================
 
-frames = [stack_rel_ret, stack_norm_size, stack_norm_valu, stack_norm_mntm]
-result = pd.concat(frames, axis=1)
-result.columns = ['RET', 'SIZE', 'VALU', 'MNTM']
+univ_regr_beta = [] # Factor betas
+univ_regr_pred = [] # Predictions
+univ_regr_rsid = [] # Residuals
 
-result.sort_index(inplace=True)
-
-factor_betas = []
-predictions = []
-residuals = []
-
-for i in result.index.levels[0][-24:]:
-    y = result.loc[(i)].RET
-    X = result.loc[(i)][['SIZE','VALU','MNTM']]
+for i in stack.index.levels[0][-24:]:
+    y = stack.loc[(i)]['pret']
+    X = stack.loc[(i)][['size','valu','mntm']]
     
     model = LinearRegression(fit_intercept=False)
     model.fit(X,y)
 
-    prediction = model.predict(X)
-    
-    residual = prediction - y
+    pred = model.predict(X)
+    rsid = pred - y
 
-    factor_betas.append(model.coef_)
-    predictions.append(prediction)
-    residuals.append(residual)
+    univ_regr_beta.append(model.coef_)
+    univ_regr_pred.append(pred)
+    univ_regr_rsid.append(rsid)
 
-factor_betas = np.array(factor_betas)
-predictions = np.array(predictions)
-residuals  = np.array(residuals)
- 
-factor_covar = np.cov(factor_betas.T)
-factor_corr = np.corrcoef(factor_betas.T)
+univ_regr_beta = np.array(univ_regr_beta)
+univ_regr_pred = np.array(univ_regr_pred)
+univ_regr_rsid = np.array(univ_regr_rsid)
 
-residuals_std = np.std(residuals, axis=0) * np.sqrt(12)
+# Factor covariance/correlation matrix
+univ_fctr_cvar = np.cov(univ_regr_beta.T)
+univ_fctr_corr = np.corrcoef(univ_regr_beta.T)
 
-# factor exposures
-norm_size_exp = norm_size.loc[DATE]
-norm_valu_exp = norm_valu.loc[DATE]
-norm_mntm_exp = norm_mntm.loc[DATE]
+univ_regr_rsid_std = np.std(univ_regr_rsid, axis=0) * np.sqrt(12)
 
-factor_exposures = np.array(pd.concat([norm_size_exp, norm_valu_exp, norm_mntm_exp], axis=1))
+# Factor exposures
+univ_norm_size_expr = fctr_size_norm.loc[DATE]
+univ_norm_valu_expr = fctr_valu_norm.loc[DATE]
+univ_norm_mntm_expr = fctr_mntm_norm.loc[DATE]
+univ_norm_fctr_expr = np.array(pd.concat([univ_norm_size_expr, univ_norm_valu_expr, univ_norm_mntm_expr], axis=1))
 
-factor_risk = np.dot(np.dot(factor_exposures, factor_covar), factor_exposures.T)
-specific_risk = np.diag(residuals_std**2)
-total_risk = factor_risk + specific_risk
+# Factor, specific, and total risk
+univ_fctr_risk = np.dot(np.dot(univ_norm_fctr_expr, univ_fctr_cvar), univ_norm_fctr_expr.T)
+univ_spec_risk = np.diag(univ_regr_rsid_std**2)
+univ_totl_risk = univ_fctr_risk + univ_spec_risk
 
-# Portfolio risk
-port_factor_risk = np.sqrt(np.dot(np.dot(port.T, factor_risk), port))
-port_specific_risk = np.sqrt(np.dot(np.dot(port.T, specific_risk), port))
-port_total_risk = np.sqrt(np.dot(np.dot(port.T, total_risk), port))
+#==============================================================================
+# Portfolio Risk
+#==============================================================================
 
-port_marginal_contrib_factor_risk = np.dot(factor_risk, port) / port_factor_risk / 100
-port_marginal_contrib_specific_risk = np.dot(specific_risk, port) / port_specific_risk / 100
-port_marginal_contrib_total_risk = np.dot(total_risk, port) / port_total_risk / 100
+# Total risk
+port_fctr_risk_totl = np.sqrt(np.dot(np.dot(port.T, univ_fctr_risk), port))
+port_spec_risk_totl = np.sqrt(np.dot(np.dot(port.T, univ_spec_risk), port))
+port_totl_risk_totl = np.sqrt(np.dot(np.dot(port.T, univ_totl_risk), port))
 
-port_contrib_factor_risk = port * port_marginal_contrib_factor_risk * 100
-port_contrib_specific_risk = port * port_marginal_contrib_specific_risk * 100
-port_contrib_total_risk = port * port_marginal_contrib_total_risk * 100
+# Marginal risk
+port_fctr_risk_marg = np.dot(univ_fctr_risk, port) / port_fctr_risk_totl / 100
+port_spec_risk_marg = np.dot(univ_spec_risk, port) / port_spec_risk_totl / 100
+port_totl_risk_marg = np.dot(univ_totl_risk, port) / port_totl_risk_totl / 100
 
-port_pct_factor_risk = port_factor_risk**2 / port_total_risk**2
-port_pct_specific_risk = port_specific_risk**2 / port_total_risk**2
-port_pct_total_risk = port_total_risk**2 / port_total_risk**2
+# Contribution to risk
+port_fctr_risk_cntr = port * port_fctr_risk_marg * 100
+port_spec_risk_cntr = port * port_spec_risk_marg * 100
+port_totl_risk_cntr = port * port_totl_risk_marg * 100
 
-port_pct_contrib_factor_risk = port_pct_factor_risk * port_contrib_factor_risk / np.sum(port_contrib_factor_risk)
-port_pct_contrib_specific_risk = port_pct_specific_risk * port_contrib_specific_risk / np.sum(port_contrib_specific_risk)
-port_pct_contrib_total_risk = port_pct_total_risk * port_contrib_total_risk / np.sum(port_contrib_total_risk)
+# Percent contribution to total risk
+port_fctr_risk_pct_cntr = (port_fctr_risk_totl**2 / port_totl_risk_totl**2) * port_fctr_risk_cntr / np.sum(port_fctr_risk_cntr)
+port_spec_risk_pct_cntr = (port_spec_risk_totl**2 / port_totl_risk_totl**2) * port_spec_risk_cntr / np.sum(port_spec_risk_cntr)
+port_totl_risk_pct_cntr = (port_totl_risk_totl**2 / port_totl_risk_totl**2) * port_totl_risk_cntr / np.sum(port_totl_risk_cntr)
 
-# Benchmark risk
-bench_factor_risk = np.sqrt(np.dot(np.dot(bench.T, factor_risk), bench))
-bench_specific_risk = np.sqrt(np.dot(np.dot(bench.T, specific_risk), bench))
-bench_total_risk = np.sqrt(np.dot(np.dot(bench.T, total_risk), bench))
+#==============================================================================
+# Benchmark Risk
+#==============================================================================
 
-bench_marginal_contrib_factor_risk = np.dot(factor_risk, bench) / bench_factor_risk / 100
-bench_marginal_contrib_specific_risk = np.dot(specific_risk, bench) / bench_specific_risk / 100
-bench_marginal_contrib_total_risk = np.dot(total_risk, bench) / bench_total_risk / 100
+# Total risk
+bench_fctr_risk_totl = np.sqrt(np.dot(np.dot(bench.T, univ_fctr_risk), bench))
+bench_spec_risk_totl = np.sqrt(np.dot(np.dot(bench.T, univ_spec_risk), bench))
+bench_totl_risk_totl = np.sqrt(np.dot(np.dot(bench.T, univ_totl_risk), bench))
 
-bench_contrib_factor_risk = bench * bench_marginal_contrib_factor_risk * 100
-bench_contrib_specific_risk = bench * bench_marginal_contrib_specific_risk * 100
-bench_contrib_total_risk = bench * bench_marginal_contrib_total_risk * 100
+# Marginal risk
+bench_fctr_risk_marg = np.dot(univ_fctr_risk, bench) / bench_fctr_risk_totl / 100
+bench_spec_risk_marg = np.dot(univ_spec_risk, bench) / bench_spec_risk_totl / 100
+bench_totl_risk_marg = np.dot(univ_totl_risk, bench) / bench_totl_risk_totl / 100
 
-bench_pct_factor_risk = bench_factor_risk**2 / bench_total_risk**2
-bench_pct_specific_risk = bench_specific_risk**2 / bench_total_risk**2
-bench_pct_total_risk = bench_total_risk**2 / bench_total_risk**2
+# Contribution to risk
+bench_fctr_risk_cntr = bench * bench_fctr_risk_marg * 100
+bench_spec_risk_cntr = bench * bench_spec_risk_marg * 100
+bench_totl_risk_cntr = bench * bench_totl_risk_marg * 100
 
-bench_pct_contrib_factor_risk = bench_pct_factor_risk * bench_contrib_factor_risk / np.sum(bench_contrib_factor_risk)
-bench_pct_contrib_specific_risk = bench_pct_specific_risk * bench_contrib_specific_risk / np.sum(bench_contrib_specific_risk)
-bench_pct_contrib_total_risk = bench_pct_total_risk * bench_contrib_total_risk / np.sum(bench_contrib_total_risk)
+# Percent contribution to total risk
+bench_fctr_risk_pct_cntr = (bench_fctr_risk_totl**2 / bench_totl_risk_totl**2) * bench_fctr_risk_cntr / np.sum(bench_fctr_risk_cntr)
+bench_spec_risk_pct_cntr = (bench_spec_risk_totl**2 / bench_totl_risk_totl**2) * bench_spec_risk_cntr / np.sum(bench_spec_risk_cntr)
+bench_totl_risk_pct_cntr = (bench_totl_risk_totl**2 / bench_totl_risk_totl**2) * bench_totl_risk_cntr / np.sum(bench_totl_risk_cntr)
 
-# Active risk
-active_factor_risk = np.sqrt(np.dot(np.dot(active.T, factor_risk), active))
-active_specific_risk = np.sqrt(np.dot(np.dot(active.T, specific_risk), active))
-active_total_risk = np.sqrt(np.dot(np.dot(active.T, total_risk), active))
+#==============================================================================
+# Active Risk
+#==============================================================================
 
-active_marginal_contrib_factor_risk = np.dot(factor_risk, active) / active_factor_risk / 100
-active_marginal_contrib_specific_risk = np.dot(specific_risk, active) / active_specific_risk / 100
-active_marginal_contrib_total_risk = np.dot(total_risk, active) / active_total_risk / 100
+# Total risk
+active_fctr_risk_totl = np.sqrt(np.dot(np.dot(active.T, univ_fctr_risk), active))
+active_spec_risk_totl = np.sqrt(np.dot(np.dot(active.T, univ_spec_risk), active))
+active_totl_risk_totl = np.sqrt(np.dot(np.dot(active.T, univ_totl_risk), active))
 
-active_contrib_factor_risk = active * active_marginal_contrib_factor_risk * 100
-active_contrib_specific_risk = active * active_marginal_contrib_specific_risk * 100
-active_contrib_total_risk = active * active_marginal_contrib_total_risk * 100
+# Marginal risk
+active_fctr_risk_marg = np.dot(univ_fctr_risk, active) / active_fctr_risk_totl / 100
+active_spec_risk_marg = np.dot(univ_spec_risk, active) / active_spec_risk_totl / 100
+active_totl_risk_marg = np.dot(univ_totl_risk, active) / active_totl_risk_totl / 100
 
-active_pct_factor_risk = active_factor_risk**2 / active_total_risk**2
-active_pct_specific_risk = active_specific_risk**2 / active_total_risk**2
-active_pct_total_risk = active_total_risk**2 / active_total_risk**2
+# Contribution to risk
+active_fctr_risk_cntr = active * active_fctr_risk_marg * 100
+active_spec_risk_cntr = active * active_spec_risk_marg * 100
+active_totl_risk_cntr = active * active_totl_risk_marg * 100
 
-active_pct_contrib_factor_risk = active_pct_factor_risk * active_contrib_factor_risk / np.sum(active_contrib_factor_risk)
-active_pct_contrib_specific_risk = active_pct_specific_risk * active_contrib_specific_risk / np.sum(active_contrib_specific_risk)
-active_pct_contrib_total_risk = active_pct_total_risk * active_contrib_total_risk / np.sum(active_contrib_total_risk)
+# Percent contribution to total risk
+active_fctr_risk_pct_cntr = (active_fctr_risk_totl**2 / active_totl_risk_totl**2) * active_fctr_risk_cntr / np.sum(active_fctr_risk_cntr)
+active_spec_risk_pct_cntr = (active_spec_risk_totl**2 / active_totl_risk_totl**2) * active_spec_risk_cntr / np.sum(active_spec_risk_cntr)
+active_totl_risk_pct_cntr = (active_totl_risk_totl**2 / active_totl_risk_totl**2) * active_totl_risk_cntr / np.sum(active_totl_risk_cntr)
+
+#==============================================================================
+# Factor risk decomposition
+#==============================================================================
 
 # Security Factor Exposures
-port_factor_exposure = np.dot(port.T, factor_exposures)
-bench_factor_exposure = np.dot(bench.T, factor_exposures)
-active_factor_exposure = np.dot(active.T, factor_exposures)
+port_fctr_expr = np.dot(port.T, univ_norm_fctr_expr)
+bench_fctr_expr = np.dot(bench.T, univ_norm_fctr_expr)
+active_fctr_expr = np.dot(active.T, univ_norm_fctr_expr)
 
 # Factor Risk Decomposition
-port_factor_decomp_marginal = np.dot(factor_covar, port_factor_exposure.T) / port_factor_risk / 100
-port_factor_decomp_contrib = port_factor_exposure.T * port_factor_decomp_marginal * 100
-port_factor_decomp_pct_contrib = port_factor_decomp_contrib / port_factor_risk
+port_fctr_dcmp_marg = np.dot(univ_fctr_cvar, port_fctr_expr.T) / port_fctr_risk_totl / 100
+port_fctr_dcmp_cntr = port_fctr_expr.T * port_fctr_dcmp_marg * 100
+port_fctr_dcmp_pct_cntr = port_fctr_dcmp_cntr / port_fctr_risk_totl
 
-bench_factor_decomp_marginal = np.dot(factor_covar, bench_factor_exposure.T) / bench_factor_risk / 100
-bench_factor_decomp_contrib = bench_factor_exposure.T * bench_factor_decomp_marginal * 100
-bench_factor_decomp_pct_contrib = bench_factor_decomp_contrib / bench_factor_risk
+bench_fctr_dcmp_marg = np.dot(univ_fctr_cvar, bench_fctr_expr.T) / bench_fctr_risk_totl / 100
+bench_fctr_dcmp_cntr = bench_fctr_expr.T * bench_fctr_dcmp_marg * 100
+bench_fctr_dcmp_pct_cntr = bench_fctr_dcmp_cntr / bench_fctr_risk_totl
 
-active_factor_decomp_marginal = np.dot(factor_covar, active_factor_exposure.T) / active_factor_risk / 100
-active_factor_decomp_contrib = active_factor_exposure.T * active_factor_decomp_marginal * 100
-active_factor_decomp_pct_contrib = active_factor_decomp_contrib / active_factor_risk
+active_fctr_dcmp_marg = np.dot(univ_fctr_cvar, active_fctr_expr.T) / active_fctr_risk_totl / 100
+active_fctr_dcmp_cntr = active_fctr_expr.T * active_fctr_dcmp_marg * 100
+active_fctr_dcmp_pct_cntr = active_fctr_dcmp_cntr / active_fctr_risk_totl
 
 #==============================================================================
 # The End
